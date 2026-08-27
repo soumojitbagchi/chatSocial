97cf91b75fc5bf7027db15437f48c0146c7c0cd5
Soumojit Bagchi <soumojitbagchi001@gmail.com>
2026-08-27 22:50:56 +0530

    refactor(present): simplify online user management by utilizing presence service

--- backend/src/sockets/handlers/present.handler.js
+++ backend/src/sockets/handlers/present.handler.js
@@ -1,27 +1,21 @@
-
-const onlineUsers = new Map();
+import * as presenceService from "../service/presence.service.js";
 
 const presentHandler = (io, socket) => {
     const userId = socket.user?.id || socket.user?._id?.toString() || socket.id;
     const username = socket.user?.username || socket.user?.name || "Anonymous";
 
     socket.on("online", () => {
-        if (!onlineUsers.has(userId)) {
-            onlineUsers.set(userId, new Set());
+        const { isFirstSocket } = presenceService.addOnlineUser(userId, socket.id);
+        if (isFirstSocket) {
             socket.broadcast.emit("user:online", { userId, username });
         }
-        onlineUsers.get(userId).add(socket.id);
-        socket.emit("users:online-list", Array.from(onlineUsers.keys()));
+        socket.emit("users:online-list", presenceService.getAllOnlineUserIds());
     });
 
     socket.on("disconnect", () => {
-        if (onlineUsers.has(userId)) {
-            const userSockets = onlineUsers.get(userId);
-            userSockets.delete(socket.id);
-            if (userSockets.size === 0) {
-                onlineUsers.delete(userId);
-                io.emit("user:offline", { userId, username });
-            }
+        const { isLastSocket } = presenceService.removeOnlineUser(userId, socket.id);
+        if (isLastSocket) {
+            io.emit("user:offline", { userId, username });
         }
     });
 };
